37cfddbb6d7fb7b2f346c7e788dbfd7d3015ff7e
EdMan1022 <EdMan1022@gmail.com>
2017-08-18 14:59:36 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='ed_stats',
+    version='0.1',
+    description='Collection of my augmented pandas classes, implementing ANOVA and machine learning methods',
+    url=''
+)
