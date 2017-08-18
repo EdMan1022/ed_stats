85bc31b4e44c5f4360038842a237b2222eb0cc2d
EdMan1022 <EdMan1022@gmail.com>
2017-08-18 15:01:50 -0400

    Finished package setup in setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ setup(
     name='ed_stats',
     version='0.1',
     description='Collection of my augmented pandas classes, implementing ANOVA and machine learning methods',
-    url=''
-)
+    url='https://github.com/EdMan1022/ed_stats.git',
+    author='EdMan1022',
+    author_email='EdMan1022@gmail.com',
+    license='MIT',
+    packages=['ed_stats'],
+    zip_safe=False)
