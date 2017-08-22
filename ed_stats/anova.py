e4d77be9ea3ce30b23edbc01b07c64b933bc6bda
EdMan1022 <edman1022@gmail.com>
2017-08-22 19:06:18 -0400

    MANOVA finally working correctly
    Signed-off-by: EdMan1022 <EdMan1022@gmail.com>

--- ed_stats/anova.py
+++ ed_stats/anova.py
@@ -18,7 +18,7 @@ def matrix_inverse(func_data):
     :param func_data: (pandas DataFrame) dataframe to be inverted
     :return: the inverse matrix as a dataframe
     """
-    return DataFrame(pinv(func_data.values), func_data.columns, func_data.index)
+    return DataFrame(pinv(func_data.values.astype(float)), func_data.columns, func_data.index)
 
 
 class AnovaDataFrame(DataFrame):
@@ -96,15 +96,20 @@ class AnovaDataFrame(DataFrame):
         else:
             columns = self.drop(labels=[group_column], axis=1).columns
 
-        data = self.dropna(axis=0, how='any', subset=columns)
+        result = DataFrame(data=None, index=columns,
+                           columns=['p_value', 'f_statistic', 'mean_variance_between', 'mean_variance_within', ])
+
+        data = self
         data.loc[:, columns] = data.loc[:, columns].astype(float)
         data.loc[:, group_column] = data.loc[:, group_column].astype(float)
-
-        data = data.dropna()
+        n_groups = data.loc[:, group_column].unique().shape[0]
 
         total_n = data.loc[:, columns[0]].count()
 
         n_vector = data.groupby(group_column)[columns[0]].count()
+
+        df1 = n_groups - 1
+        df2 = total_n - n_groups
 
         grand_mean_vector = (data.groupby(group_column)[columns].agg(weighted_sum)).sum() / total_n
 
@@ -113,19 +118,19 @@ class AnovaDataFrame(DataFrame):
         for dependent_variable in columns:
             sample_mean_matrix.loc[:, dependent_variable] = data.groupby(group_column)[dependent_variable].mean()
 
-        total_variance = data.cov() * (total_n - 1)
+        total_variance = data.loc[:, columns].cov() * (total_n - 1)
 
-        hypothesis_variance = (sample_mean_matrix - grand_mean_vector).dot(
-            (sample_mean_matrix - grand_mean_vector).T.dot(n_vector))
+        hypothesis_variance = ((sample_mean_matrix - grand_mean_vector).T*(n_vector)).dot(
+            (sample_mean_matrix - grand_mean_vector))
 
         error_variance = total_variance - hypothesis_variance
 
         wilks_lambda = norm(error_variance.values)/norm(total_variance.values)
 
-        hotelling_lawley_trace = trace(hypothesis_variance.dot(error_variance.inverse()))
+        hotelling_lawley_trace = trace(hypothesis_variance.dot(matrix_inverse(error_variance)))
 
         phillai_bartlett_trace = trace(hypothesis_variance.dot(
-            (hypothesis_variance + error_variance).inverse()
+            matrix_inverse(hypothesis_variance + error_variance)
         ))
 
         return wilks_lambda, hotelling_lawley_trace, phillai_bartlett_trace
